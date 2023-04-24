7db7f951c3cef37fde0e5a4a1450d470961cdf26
Fabio Morea <fabio.morea@areasciencepark.it>
2023-04-24 11:51:17 +0200

    community size

--- 5_plots_consensus.r
+++ 5_plots_consensus.r
@@ -1,44 +1,50 @@
  
-library(ggplot2)
-library(tidyverse)
+library(tidyverse, warn.conflicts = FALSE)
+library(jsonlite) 
 
+json_data <- fromJSON("results.JSON")
+cols_to_extract <- c('mu', 'nmi', 'method', "a")
+tmp_list <- list()
+nn <- length(cols_to_extract)
+for (i in 1:nn){
+    tmp_list[[i]] <- pluck(json_data, cols_to_extract[i]) 
+}
+df <- data.frame(tmp_list)
+names(df) <- cols_to_extract
+df$mbs <- pluck(json_data, "membership")
 
+for (i in (1:nrow(df))){
+    com_sizes <- as.vector(table(df$mbs[1]))
+    com_sizes <- sort(com_sizes, decreasing = TRUE)
+    com_sizes <- c(com_sizes, rep(NA, 100))
+    com_sizes <- com_sizes[1:100]
+    df$cs[i] <- list(com_sizes)
+}
 
-## community size distribution on consensus
-plot_comm_dist <- function(filename){
+len <- lengths(df$nmi)
+df$id <- seq.int(nrow(df))
 
-    mm  <- read.csv(file = filename)
-    df3 <-  data.frame(x = rep(1, 200))
-    for (i in 1:ncol(mm)){ 
-        com_sizes <- as.vector( table(mm[,i]) )
-        com_sizes <- sort(com_sizes, decreasing = FALSE)
-        com_sizes <- c(com_sizes, rep(NA, 100))
-        com_sizes <- com_sizes[1:100]
-        df3[,i] <- com_sizes
-    }
-    #df3 <- df3[,] 
-    print(head(df3))
+df1 <- data.frame(  nmi = rep(df$nmi, len ),
+                    id = rep(df$id, len ),
+                    cs = unlist(df$cs),
+                    a = rep(df$a, len ),
+                    mu = rep(df$mu, len )
+                    ) %>%
+        filter(a == 0) %>% 
+        group_by(id) %>%
+        arrange(id, cs) %>%  
+        na.omit()   
 
+print(head(df1))
+#print(head(df))
     
-    p4 <- ggplot(df3, aes(x = x, y=V2))+
-        geom_line(color='gray', size = 2, alpha = 0.2)+
-        geom_point(color='red', size = 2, alpha = 0.2)+
-        theme_light() +
-        ylim(0, 100)  + 
-        scale_x_discrete(labels= NULL)+
-        xlab("") + ggtitle(paste("CONSENSUS community size distribution", filename))
+p4 <- ggplot(df1, aes(x = nmi, y=cs, group = id, color = mu))+
+    geom_line( size = 2, alpha = 0.2)+
+    geom_point( size = 2, alpha = 0.2)+
+    theme_light()  +
+    facet_grid(rows = vars(mu)) 
 
-    print(p4)
-    ggsave(paste0("results/plot_comm_size", filename, ".png"))
+print(p4) 
+ggsave("plot comm distrib.png")
 
-    p4h <- ggplot(df3, aes(  x=V2))+
-    geom_histogram(bins = 4, color = 'white', fill = 'red')+
-    theme_light() 
-    print(p4h)
-    ggsave(width = 6, height = 3,"results/plot_comm_size_hist.png")
 
-}
-for (mui in seq(10,99,10)){
-    plot_comm_dist(paste0("results/cons_labels", mui, ".csv"))
-
-}
